1d703032041bdeea5f8c2333e9b4b2b805febc94
nico604 <nicolas.girard.qc@gmail.com>
2022-04-20 18:26:16 -0400

    setup game

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { PureComponent } from "react";
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import { GameEngine } from "react-game-engine";
+import { Box } from "./renderers";
+import { MoveBox } from "./systems"
+
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+     <GameEngine
+        style={{ width: 800, height: 600, backgroundColor: "blue" }}
+        systems={[MoveBox]}
+        entities={{
+          //-- Notice that each entity has a unique id (required)
+          //-- and a renderer property (optional). If no renderer
+          //-- is supplied with the entity - it won't get displayed.
+          box1: { x: 200,  y: 200, renderer: <Box />}
+        }}>
+
+      </GameEngine>
+
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
