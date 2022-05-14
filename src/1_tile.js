fd7fae5f173016dc07824f5663eee971d88f3fab
nico604 <nicolas.girard.qc@gmail.com>
2022-05-14 10:33:03 -0400

    ecran accueil v1

---
+++ src/1_tile.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  overflow: hidden;
+  transform: scale(${({ scale }) => `${scale}, ${scale}`});
+  transform-origin: top left;
+  margin: 0 0 3em;
+`;
+
+const Image = styled.img`
+    transform: translate(-${({ left }) => left}px, 0);
+`;
+
+export default class Tile extends React.Component {
+  static propTypes = {
+    src: PropTypes.string.isRequired,
+    tile: PropTypes.object.isRequired,
+    state: PropTypes.number.isRequired,
+    scale: PropTypes.number.isRequired
+  };
+
+static defaultProps = {
+    scale: 1
+  };
+
+  render() {
+    const { src, tile, state, scale } = this.props;
+
+    // TODO: use tile width and state properties
+    // to calculate left position of the image
+    const left = tile.width * state;
+    return (
+      <Container width={tile.width} height={tile.height} scale={scale}>
+        <Image src={src} left={left} />
+      </Container>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React, { PureComponent } from "react";
 import ReactDOM from 'react-dom/client';
+import styled from "styled-components";
+import Tile from "./1_tile";
+import sideAnimation from "./assets/side.png";
+import Sprite from "./2_sprite";
 import reportWebVitals from './reportWebVitals';
-
 import Bienvenue from "./components/Bienvenue/Bienvenue";
 import moi1 from './assets/sprite1.gif';
 import moi2 from './assets/sprite2.gif';
+import styles from './index.css';
+
+const tile = { width: 20, height: 24 };
+const Container = styled.div`
+  height: 50px;
+`;
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+function entrerApp() {
+  alert('Ok allé go!');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <Bienvenue nom="Nathan" age={27} occupation="Dj" img={moi1}></Bienvenue>
-     <Bienvenue nom="Tristan" age={32} occupation="Star" img={moi2}></Bienvenue>
-       </div>  
+     <Container id="container">
+       <div id="ecranPremier">
+        <h1>Bienvenue</h1>
+        <Sprite
+          src={sideAnimation}
+          tile={tile}
+          states={4}
+          scale={2.5}
+          framesPerStep={8}
+        />
+        <Button onClick={entrerApp}>
+          Entrer app
+        </Button>
+      </div>
+    </Container>
 );
